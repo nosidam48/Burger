d775615b937a1618a8d69d0a23af299ca16498e0
Mad Tea <43210154+nosidam48@users.noreply.github.com>
2018-12-21 10:03:37 -0500

    JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,37 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 8889,
-//   user: "root",
-//   password: "root",
-//   database: "burgers_db"
-// });
-
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
-  host     : 'us-cdbr-iron-east-01.cleardb.net',
-  user     : 'b3e394f661c85b',
-  password : '0d221778',
-  database : 'heroku_21ebb01b5547d53'
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+
 });
+}
 
-connection.connect();
-
-
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
