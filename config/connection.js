cd586633962d793c27607c2b9bf7c8085a7d4200
Mad Tea <43210154+nosidam48@users.noreply.github.com>
2018-12-20 20:30:09 -0500

    cleardb2?

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,37 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+// var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 8889,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+  host     : 'us-cdbr-iron-east-01.cleardb.net',
+  user     : 'b3e394f661c85b',
+  password : '0d221778',
+  database : 'heroku_21ebb01b5547d53'
 });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect();
+
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
 
 // Export connection for our ORM to use.
